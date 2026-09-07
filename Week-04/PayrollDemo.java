d576db332725920aa0a9ffa2d296ab434aa00b66
m9nihal-max <m9nihal@gmail.com>
2026-09-07 19:35:18 +0530

    PayRoll demo

---
+++ Week-04/PayrollDemo.java
@@ -0,0 +1,39 @@
+class Employee {
+
+    String empId;
+    double salary;
+
+    Employee(String empId, double salary) {
+        this.empId = empId;
+        this.salary = salary;
+    }
+
+    void raiseSalary(double salary) {
+        this.salary = this.salary + salary;
+    }
+}
+
+public class PayrollDemo {
+
+    public static void main(String[] args) {
+
+        Employee[] employees = {
+            new Employee("E-101", 40000),
+            new Employee("E-102", 55000),
+            new Employee("E-103", 62000),
+            new Employee("E-104", 48000)
+        };
+
+        double bonus = 5000;
+
+        for (int i = 0; i < employees.length; i++) {
+            employees[i].raiseSalary(bonus);
+
+            System.out.println(
+                employees[i].empId +
+                " | Final Salary: Rs " +
+                employees[i].salary
+            );
+        }
+    }
+}
