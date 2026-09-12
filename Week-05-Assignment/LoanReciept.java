4835790bb79acf985acba59b7df2dae0f6736486
m9nihal-max <m9nihal@gmail.com>
2026-09-12 14:41:30 +0530

    Implement LoanReceipt class for book loans

---
+++ Week-05-Assignment/LoanReciept.java
@@ -0,0 +1,23 @@
+public final class LoanReceipt {
+
+    private final String memberId;
+    private final String[] bookIds;
+
+    public LoanReceipt(String memberId, String[] bookIds) {
+        this.memberId = memberId;
+        this.bookIds = bookIds.clone();
+    }
+
+    public String[] getBookIds() {
+        return bookIds.clone();
+    }
+
+    public LoanReceipt withCorrectedBookId(int index, String newId) {
+
+        String[] correctedIds = bookIds.clone();
+
+        correctedIds[index] = newId;
+
+        return new LoanReceipt(memberId, correctedIds);
+    }
+}
